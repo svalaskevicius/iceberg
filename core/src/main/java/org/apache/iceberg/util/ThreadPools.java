851f819006dcd62358309ec06f7bdeade9933305
Sarunas Valaskevicius <rakatan@gmail.com>
2026-02-13 16:49:24 +0000

    add more security exception catching / logging

--- core/src/main/java/org/apache/iceberg/util/ThreadPools.java
+++ core/src/main/java/org/apache/iceberg/util/ThreadPools.java
@@ -197,11 +197,14 @@ public class ThreadPools {
       try {
         shutdownHook.setName("DelayedShutdownHook-iceberg");
       } catch (SecurityException e) {
-        // OK if we can't set the name in this environment.
         LOG.warn("Cannot set thread name for the shutdown hook", e);
       }
 
-      Runtime.getRuntime().addShutdownHook(shutdownHook);
+      try {
+        Runtime.getRuntime().addShutdownHook(shutdownHook);
+      } catch (SecurityException e) {
+        LOG.warn("Cannot install a shutdown hook for thread pools clean up", e);
+      }
     }
   }
 
@@ -215,7 +218,11 @@ public class ThreadPools {
   @SuppressWarnings("ShutdownHook")
   public static void removeShutdownHook() {
     if (shutdownHook != null) {
-      Runtime.getRuntime().removeShutdownHook(shutdownHook);
+      try {
+        Runtime.getRuntime().removeShutdownHook(shutdownHook);
+      } catch (SecurityException e) {
+        LOG.warn("Cannot remove the shutdown hook for thread pools clean up", e);
+      }
       shutdownHook = null;
     }
   }
