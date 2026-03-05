7587f76ef14bbda6782637be9a7f8f9dd27658a2
Sarunas Valaskevicius <rakatan@gmail.com>
2026-03-05 19:34:30 +0000

    remove comment as we have log message now

--- core/src/main/java/org/apache/iceberg/util/ThreadPools.java
+++ core/src/main/java/org/apache/iceberg/util/ThreadPools.java
@@ -292,7 +292,6 @@ public class ThreadPools {
               item.getService().shutdownNow();
             }
           } catch (InterruptedException e) {
-            // We're shutting down anyway, so just ignore.
             LOG.warn("Interrupted while shutting down, ignoring", e);
           }
 
